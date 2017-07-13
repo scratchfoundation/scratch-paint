836088a8cbb0d50ac025fa79cfabd9ea843b1766
DD Liu <liudi@media.mit.edu>
2017-07-13 14:47:23 -0400

    Remove VM dependency

--- src/components/paint-editor.jsx
+++ src/components/paint-editor.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import VM from 'scratch-vm';
 
 export default class PaintEditorComponent extends React.Component {
     render () {
         return (
             <div className="paint-editor">
-            	BANANAS
+                BANANAS
             </div>
         );
-     }
+    }
 }
 
 PaintEditorComponent.defaultProps = {
-    vm: new VM()
 };
