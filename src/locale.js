11d43e000665cb4f873368fd246164b1390434a6
DD Liu <liudi@media.mit.edu>
2017-08-21 17:58:38 -0400

    add i18n support

---
+++ src/locale.js
@@ -0,0 +1,11 @@
+import localeDataEn from 'react-intl/locale-data/en';
+
+import messages from '../locale/messages.json'; // eslint-disable-line import/no-unresolved
+
+export default {
+    en: {
+        name: 'English',
+        localeData: localeDataEn,
+        messages: messages.en
+    }
+};
--- src/playground/playground.jsx
+++ src/playground/playground.jsx
@@ -4,15 +4,19 @@ import PaintEditor from '..';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducer from '../reducers/combine-reducers';
+import {intlInitialState, IntlProvider} from '../reducers/intl.js';
 
 const appTarget = document.createElement('div');
 document.body.appendChild(appTarget);
 const store = createStore(
     reducer,
+    intlInitialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 ReactDOM.render((
     <Provider store={store}>
-        <PaintEditor />
+        <IntlProvider>
+            <PaintEditor />
+        </IntlProvider>
     </Provider>
 ), appTarget);
