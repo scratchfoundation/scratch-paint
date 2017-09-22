7e1375d8554aad3219e8f8401c446f5d7d34ba75
DD <liudi08@gmail.com>
2017-09-22 13:48:18 -0400

    Switch state to track hover item ID instead of item itself

--- src/reducers/scratch-paint-reducer.js
+++ src/reducers/scratch-paint-reducer.js
@@ -10,5 +10,5 @@ export default combineReducers({
     brushMode: brushModeReducer,
     eraserMode: eraserModeReducer,
     color: colorReducer,
-    hoveredItem: hoverReducer
+    hoveredItemId: hoverReducer
 });
