60a310ae853de0a8092b9e44e59ad461a8df7034
DD <liudi08@gmail.com>
2017-10-03 18:13:12 -0400

    remove unused

--- src/containers/selection-hoc.jsx
+++ src/containers/selection-hoc.jsx
@@ -5,12 +5,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 import bindAll from 'lodash.bindall';
 
-import {getSelectedItems} from '../helper/selection';
-import {getColorsFromSelection} from '../helper/style-path';
-import {changeStrokeColor} from '../reducers/stroke-color';
-import {changeStrokeWidth} from '../reducers/stroke-width';
-import {changeFillColor} from '../reducers/fill-color';
-
 const SelectionHOC = function (WrappedComponent) {
     class SelectionComponent extends React.Component {
         constructor (props) {
@@ -59,21 +53,8 @@ const SelectionHOC = function (WrappedComponent) {
     const mapStateToProps = state => ({
         hoveredItemId: state.scratchPaint.hoveredItemId
     });
-    const mapDispatchToProps = dispatch => ({
-        onUpdateColors: (() => {
-            const selectedItems = getSelectedItems(true /* recursive */);
-            if (selectedItems.length === 0) {
-                return;
-            }
-            const colorState = getColorsFromSelection(selectedItems);
-            dispatch(changeFillColor(colorState.fillColor));
-            dispatch(changeStrokeColor(colorState.strokeColor));
-            dispatch(changeStrokeWidth(colorState.strokeWidth));
-        })
-    });
     return connect(
-        mapStateToProps,
-        mapDispatchToProps
+        mapStateToProps
     )(SelectionComponent);
 };
 
