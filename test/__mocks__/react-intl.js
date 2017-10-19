e2f70a68ba2cdd19d14d52ccdae85f53fec424e5
Matthew Taylor <mewtaylor@gmail.com>
2017-10-19 16:55:22 -0400

    Add mock for react-intl

---
+++ test/__mocks__/react-intl.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const Intl = require.requireActual('react-intl');
+
+// Here goes intl context injected into component, feel free to extend
+const intl = {
+    formatMessage: ({defaultMessage}) => defaultMessage,
+    formatDate: ({defaultMessage}) => defaultMessage,
+    formatTime: ({defaultMessage}) => defaultMessage,
+    formatRelative: ({defaultMessage}) => defaultMessage,
+    formatNumber: ({defaultMessage}) => defaultMessage,
+    formatPlural: ({defaultMessage}) => defaultMessage,
+    formatHTMLMessage: ({defaultMessage}) => defaultMessage,
+    now: () => 0
+};
+
+Intl.injectIntl = Node => {
+    const renderWrapped = props => <Node {...props} intl={intl} />;
+    renderWrapped.displayName = Node.displayName ||
+        Node.name ||
+        'Component';
+    return renderWrapped;
+};
+
+module.exports = Intl;
