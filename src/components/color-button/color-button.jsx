d98a493954ba113f14b63d3c9aefd79986c685f7
Paul Kaplan <pkaplan@media.mit.edu>
2017-10-26 15:52:37 -0400

    Add initial transparent and mixed icons for color buttons and swatch

--- src/components/color-button/color-button.jsx
+++ src/components/color-button/color-button.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {MIXED} from '../../helper/style-path';
+
+import noFillIcon from './no-fill.svg';
+import mixedFillIcon from './mixed-fill.svg';
 import styles from './color-button.css';
+
+const colorToBackground = (color) => {
+    return color === MIXED || color === null ? 'white' : color
+};
 
 const ColorButtonComponent = props => (
     <div
@@ -11,9 +19,21 @@ const ColorButtonComponent = props => (
         <div
             className={styles.colorButtonSwatch}
             style={{
-                background: props.color
+                background: colorToBackground(props.color)
             }}
-        />
+        >
+            {props.color === null ? (
+                <img
+                    className={styles.swatchIcon}
+                    src={noFillIcon}
+                />
+            ) : ((props.color === MIXED ? (
+                <img
+                    className={styles.swatchIcon}
+                    src={mixedFillIcon}
+                />
+            ) : null))}
+        </div>
         <div className={styles.colorButtonArrow}>▾</div>
     </div>
 );
