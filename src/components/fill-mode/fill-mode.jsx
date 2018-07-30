cdc683add3505fccee97361f824aa2d8b30c76bd
apple502j <33279053+apple502j@users.noreply.github.com>
2018-07-30 10:21:14 +0900

    Fill Translation

--- src/components/fill-mode/fill-mode.jsx
+++ src/components/fill-mode/fill-mode.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import fillIcon from './fill.svg';
 
+const messages = defineMessages({
+    fill: {
+        defaultMessage: 'Fill',
+        description: 'Label for the fill tool',
+        id: 'paint.fillMode.fill'
+    }
+}); 
+
 const FillModeComponent = props => (
     <ToolSelectComponent
-        imgDescriptor={{
-            defaultMessage: 'Fill',
-            description: 'Label for the fill tool',
-            id: 'paint.fillMode.fill'
-        }}
+        imgDescriptor={messages.fill}
         imgSrc={fillIcon}
         isSelected={props.isSelected}
         onMouseDown={props.onMouseDown}
