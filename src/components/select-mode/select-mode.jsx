1ecf5caddeb078ce2cea1df4b8b07dbe94f9c64d
apple502j <33279053+apple502j@users.noreply.github.com>
2018-07-30 10:51:33 +0900

    Select Translation

--- src/components/select-mode/select-mode.jsx
+++ src/components/select-mode/select-mode.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import selectIcon from './select.svg';
 
+const messages = defineMessages({
+    select: {
+        defaultMessage: 'Select',
+        description: 'Label for the select tool, which allows selecting, moving, and resizing shapes',
+        id: 'paint.selectMode.select'
+    }
+}); 
+
 const SelectModeComponent = props => (
     <ToolSelectComponent
-        imgDescriptor={{
-            defaultMessage: 'Select',
-            description: 'Label for the select tool, which allows selecting, moving, and resizing shapes',
-            id: 'paint.selectMode.select'
-        }}
+        imgDescriptor={messages.select}
         imgSrc={selectIcon}
         isSelected={props.isSelected}
         onMouseDown={props.onMouseDown}
