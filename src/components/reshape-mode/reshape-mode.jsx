6b396c247389bd2f66bd49e7d097289ee68d8ae6
apple502j <33279053+apple502j@users.noreply.github.com>
2018-07-30 10:48:01 +0900

    Reshape Translation

--- src/components/reshape-mode/reshape-mode.jsx
+++ src/components/reshape-mode/reshape-mode.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import reshapeIcon from './reshape.svg';
 
+const messages = defineMessages({
+    reshape: {
+        defaultMessage: 'Reshape',
+        description: 'Label for the reshape tool, which allows changing the points in the lines of the vectors',
+        id: 'paint.reshapeMode.reshape'
+    }
+}); 
+
 const ReshapeModeComponent = props => (
     <ToolSelectComponent
-        imgDescriptor={{
-            defaultMessage: 'Reshape',
-            description: 'Label for the reshape tool, which allows changing the points in the lines of the vectors',
-            id: 'paint.reshapeMode.reshape'
-        }}
+        imgDescriptor={messages.reshape}
         imgSrc={reshapeIcon}
         isSelected={props.isSelected}
         onMouseDown={props.onMouseDown}
