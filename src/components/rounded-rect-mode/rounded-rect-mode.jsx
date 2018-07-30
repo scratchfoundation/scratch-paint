e1d42c71ebc4e99db38fda2099942ab690c7c4de
apple502j <33279053+apple502j@users.noreply.github.com>
2018-07-30 10:49:45 +0900

    RoundedRect Translation

--- src/components/rounded-rect-mode/rounded-rect-mode.jsx
+++ src/components/rounded-rect-mode/rounded-rect-mode.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import roundedRectIcon from './rounded-rectangle.svg';
 
+const messages = defineMessages({
+    roundedRect: {
+        defaultMessage: 'Rounded Rectangle',
+        description: 'Label for the rounded rectangle tool',
+        id: 'paint.roundedRectMode.roundedRect'
+    }
+}); 
+
 const RoundedRectModeComponent = props => (
     <ToolSelectComponent
-        imgDescriptor={{
-            defaultMessage: 'Rounded Rectangle',
-            description: 'Label for the rounded rectangle tool',
-            id: 'paint.roundedRectMode.roundedRect'
-        }}
+        imgDescriptor={messages.roundedRect}
         imgSrc={roundedRectIcon}
         isSelected={props.isSelected}
         onMouseDown={props.onMouseDown}
