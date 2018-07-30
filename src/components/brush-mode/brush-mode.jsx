38a2380839cc5810a7f3de84eeb6c13909ba2dad
apple502j <33279053+apple502j@users.noreply.github.com>
2018-07-30 10:17:19 +0900

    Brush Translation

--- src/components/brush-mode/brush-mode.jsx
+++ src/components/brush-mode/brush-mode.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import brushIcon from './brush.svg';
 
+const messages = defineMessages({
+    brush: {
+        defaultMessage: 'Brush',
+        description: 'Label for the brush tool',
+        id: 'paint.brushMode.brush'
+    }
+}); 
+
 const BrushModeComponent = props => (
     <ToolSelectComponent
-        imgDescriptor={{
-            defaultMessage: 'Brush',
-            description: 'Label for the brush tool',
-            id: 'paint.brushMode.brush'
-        }}
+        imgDescriptor={messages.brush}
         imgSrc={brushIcon}
         isSelected={props.isSelected}
         onMouseDown={props.onMouseDown}
