163f5014a0f1c01771a1795bf56b3ff126aab295
apple502j <33279053+apple502j@users.noreply.github.com>
2018-07-30 10:15:35 +0900

    Bitmap Text translation

--- src/components/bit-text-mode/bit-text-mode.jsx
+++ src/components/bit-text-mode/bit-text-mode.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {defineMessages} from 'react-intl';
 
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import textIcon from './text.svg';
 
+const messages = defineMessages({
+    text: {
+        defaultMessage: 'Text',
+        description: 'Label for the text tool',
+        id: 'paint.textMode.text'
+    }
+}); 
+
 const BitTextComponent = props => (
     <ToolSelectComponent
-        imgDescriptor={{
-            defaultMessage: 'Text',
-            description: 'Label for the text tool',
-            id: 'paint.textMode.text'
-        }}
+        imgDescriptor={messages.text}
         imgSrc={textIcon}
         isSelected={props.isSelected}
         onMouseDown={props.onMouseDown}
