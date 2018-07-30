3e677416bd016c636db4b6399b369af1009d17ba
apple502j <33279053+apple502j@users.noreply.github.com>
2018-07-30 10:12:05 +0900

    Bitmap Select Translation

--- src/components/bit-select-mode/bit-select-mode.jsx
+++ src/components/bit-select-mode/bit-select-mode.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import selectIcon from './marquee.svg';
+
+const messages = defineMessages({
+    select: {
+        defaultMessage: 'Select',
+        description: 'Label for the select tool, which allows selecting, moving, and resizing shapes',
+        id: 'paint.selectMode.select'
+    }
+}); 
 
 const BitSelectComponent = props => (
     <ToolSelectComponent
