2284c768805b0ba1f96fbe4f6dbf5c3db0a0a686
apple502j <33279053+apple502j@users.noreply.github.com>
2018-08-02 16:26:05 +0900

    use messages.js

--- src/components/text-mode/text-mode.jsx
+++ src/components/text-mode/text-mode.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {defineMessages} from 'react-intl';
+import {messages} from '../../lib/messages.js';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
 
 import textIcon from './text.svg';
-
-const messages = defineMessages({
-    text: {
-        defaultMessage: 'Text',
-        description: 'Label for the text tool',
-        id: 'paint.textMode.text'
-    }
-});
 
 const TextModeComponent = props => (
     <ToolSelectComponent
