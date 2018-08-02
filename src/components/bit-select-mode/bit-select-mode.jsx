887af7958dbb5c8ee6af4186b9c3cc5175a1a11f
apple502j <33279053+apple502j@users.noreply.github.com>
2018-08-02 16:18:21 +0900

    Use messages.js

--- src/components/bit-select-mode/bit-select-mode.jsx
+++ src/components/bit-select-mode/bit-select-mode.jsx
@@ -2,16 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
-
+import {messages} from '../../lib/messages.js';
 import selectIcon from './marquee.svg';
-
-const messages = defineMessages({
-    select: {
-        defaultMessage: 'Select',
-        description: 'Label for the select tool, which allows selecting, moving, and resizing shapes',
-        id: 'paint.selectMode.select'
-    }
-});
 
 const BitSelectComponent = props => (
     <ToolSelectComponent
