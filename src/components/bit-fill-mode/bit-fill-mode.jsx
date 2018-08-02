7eff6a87f6d4cb65894bf340a609c3f3e220e418
apple502j <33279053+apple502j@users.noreply.github.com>
2018-08-02 17:24:18 +0900

    Lint again

--- src/components/bit-fill-mode/bit-fill-mode.jsx
+++ src/components/bit-fill-mode/bit-fill-mode.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
-import {messages} from '../../lib/messages.js';
+import messages from '../../lib/messages.js';
 import fillIcon from './fill.svg';
 
 const BitFillComponent = props => (
