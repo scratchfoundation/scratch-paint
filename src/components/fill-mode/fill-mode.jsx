fcd94c768943742307306366f79c52443edb19b9
apple502j <33279053+apple502j@users.noreply.github.com>
2018-08-02 16:21:27 +0900

    Use messages.js

--- src/components/fill-mode/fill-mode.jsx
+++ src/components/fill-mode/fill-mode.jsx
@@ -2,16 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {defineMessages} from 'react-intl';
 import ToolSelectComponent from '../tool-select-base/tool-select-base.jsx';
-
+import {messages} from '../../lib/messages.js';
 import fillIcon from './fill.svg';
-
-const messages = defineMessages({
-    fill: {
-        defaultMessage: 'Fill',
-        description: 'Label for the fill tool',
-        id: 'paint.fillMode.fill'
-    }
-});
 
 const FillModeComponent = props => (
     <ToolSelectComponent
