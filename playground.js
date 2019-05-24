6c5d21de895a5fb00c7f5cbf1082fb267d9c8d7a
Paul <pkaplan@media.mit.edu>
2019-05-24 13:33:22 +0000

    Build for 8e91dcd7dc18e841edb1fd0a4066ff2e95777a44

--- playground.js
+++ playground.js
@@ -31080,6 +31080,12 @@ const {FONTS} = __webpack_require__(215);
  * @return {string} The svg with any needed fonts inlined
  */
 const inlineSvgFonts = function (svgString) {
+    // Make it clear that this function only operates on strings.
+    // If we don't explicitly throw this here, the function silently fails.
+    if (typeof svgString !== 'string') {
+        throw new Error('SVG to be inlined is not a string');
+    }
+
     // Collect fonts that need injection.
     const fontsNeeded = new Set();
     const fontRegex = /font-family="([^"]*)"/g;
