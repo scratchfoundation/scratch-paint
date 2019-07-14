98a737857a8545b60e8940dbed65cfabfe9dbccf
Matthias Ableidinger <ableimat@gmx.at>
2019-07-14 23:59:31 +0200

    center bitmaps
    * when you just created a new shape it is still buggy

--- src/helper/center.js
+++ src/helper/center.js
@@ -1,21 +1,42 @@
 import paper from '@scratch/paper';
-import { clearSelection} from './selection';
-import {ART_BOARD_WIDTH, ART_BOARD_HEIGHT} from './view';
+import { clearSelection } from './selection';
+import { getHitBounds } from './bitmap';
+import { ART_BOARD_WIDTH, ART_BOARD_HEIGHT } from './view';
+import {createCanvas, clearRaster, getRaster, hideGuideLayers, showGuideLayers} from './layer';
 
 const center = function (onUpdateImage, isBitmap, clearSelectedItems) {
+    clearSelection(clearSelectedItems);
     if (isBitmap) {
-        console.log("bitmap");
-    }  else{
-        clearSelection(clearSelectedItems);
-        window.paper = paper;
-        const bounds = paper.project.activeLayer.internalBounds;
-        const centerSprite = new paper.Point((bounds.x * 2 + bounds.width) / 2 , (bounds.y * 2 + bounds.height) / 2);
-        const centerBoard = new paper.Point(ART_BOARD_WIDTH /2 , ART_BOARD_HEIGHT /2);
-        const translateVector = centerBoard.subtract(centerSprite);
-        paper.project.activeLayer.translate(translateVector);
-    } 
+        _centerBitmap();
+    } else {
+        _centerVector();
+    }
     onUpdateImage();
 };
 export {
     center
 };
+
+function _centerVector() {
+    const translateVector = _getTranslateVector(paper.project.activeLayer.internalBounds);
+    paper.project.activeLayer.translate(translateVector);
+}
+
+function _centerBitmap() {
+    const canvas = getRaster().canvas;
+    const translateVector = _getTranslateVector(getHitBounds(getRaster()));
+
+    const tmpCanvas = createCanvas(canvas.width, canvas.height);
+    const context = tmpCanvas.getContext('2d');
+    context.save();
+    context.translate(translateVector.x, translateVector.y);
+    context.drawImage(canvas, 0, 0);
+    context.restore();
+    getRaster().canvas = tmpCanvas;
+}
+
+function _getTranslateVector(bounds){
+    const centerSprite = new paper.Point((bounds.x * 2 + bounds.width) / 2, (bounds.y * 2 + bounds.height) / 2);
+    const centerBoard = new paper.Point(ART_BOARD_WIDTH / 2, ART_BOARD_HEIGHT / 2);
+    return centerBoard.subtract(centerSprite);
+} 
