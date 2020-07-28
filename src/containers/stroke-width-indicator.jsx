cca0832f0dd6ad8fe07717468da72bb901e87843
adroitwhiz <adroitwhiz@protonmail.com>
2020-07-28 08:36:34 -0400

    Fix zero-width-outline logic
    - Set the stroke color to "null" when the width is set to 0
    - Properly set the stroke color state when the width is increased from 0

--- src/containers/stroke-width-indicator.jsx
+++ src/containers/stroke-width-indicator.jsx
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import bindAll from 'lodash.bindall';
 import parseColor from 'parse-color';
-import {changeStrokeColor} from '../reducers/stroke-style';
+import {changeStrokeColor, changeStrokeColor2, changeStrokeGradientType} from '../reducers/stroke-style';
 import {changeStrokeWidth} from '../reducers/stroke-width';
 import StrokeWidthIndicatorComponent from '../components/stroke-width-indicator.jsx';
 import {getSelectedLeafItems} from '../helper/selection';
 import {applyColorToSelection, applyStrokeWidthToSelection, getColorsFromSelection, MIXED}
     from '../helper/style-path';
+import GradientTypes from '../lib/gradient-types';
 import Modes from '../lib/modes';
 import Formats from '../lib/format';
 import {isBitmap} from '../lib/format';
@@ -23,8 +24,15 @@ class StrokeWidthIndicator extends React.Component {
     handleChangeStrokeWidth (newWidth) {
         let changed = applyStrokeWidthToSelection(newWidth, this.props.textEditTarget);
         if ((!this.props.strokeWidth || this.props.strokeWidth === 0) && newWidth > 0) {
-            let currentColor = getColorsFromSelection(getSelectedLeafItems(), isBitmap(this.props.format)).strokeColor;
-            if (currentColor === null) {
+            const currentColorState = getColorsFromSelection(getSelectedLeafItems(), isBitmap(this.props.format));
+
+            // Color counts as null if either both colors are null or the primary color is null and it's solid
+            // TODO: consolidate this check in one place
+            const wasNull = currentColorState.strokeColor === null &&
+                (currentColorState.strokeColor2 === null ||
+                 currentColorState.strokeGradientType === GradientTypes.SOLID);
+
+            if (wasNull) {
                 changed = applyColorToSelection(
                     '#000',
                     0, // colorIndex,
@@ -32,11 +40,15 @@ class StrokeWidthIndicator extends React.Component {
                     true, // applyToStroke
                     this.props.textEditTarget) ||
                     changed;
-                currentColor = '#000';
-            } else if (currentColor !== MIXED) {
-                currentColor = parseColor(currentColor).hex;
+                // If there's no previous stroke color, default to solid black
+                this.props.onChangeStrokeGradientType(GradientTypes.SOLID);
+                this.props.onChangeStrokeColor('#000');
+            } else if (currentColorState.strokeColor !== MIXED) {
+                // Set color state from the selected item's stroke color
+                this.props.onChangeStrokeGradientType(currentColorState.strokeGradientType);
+                this.props.onChangeStrokeColor(parseColor(currentColorState.strokeColor).hex);
+                this.props.onChangeStrokeColor2(parseColor(currentColorState.strokeColor2).hex);
             }
-            this.props.onChangeStrokeColor(currentColor);
         }
         this.props.onChangeStrokeWidth(newWidth);
         if (changed) this.props.onUpdateImage();
@@ -64,6 +76,12 @@ const mapDispatchToProps = dispatch => ({
     onChangeStrokeColor: strokeColor => {
         dispatch(changeStrokeColor(strokeColor));
     },
+    onChangeStrokeColor2: strokeColor => {
+        dispatch(changeStrokeColor2(strokeColor));
+    },
+    onChangeStrokeGradientType: strokeColor => {
+        dispatch(changeStrokeGradientType(strokeColor));
+    },
     onChangeStrokeWidth: strokeWidth => {
         dispatch(changeStrokeWidth(strokeWidth));
     }
@@ -73,6 +91,8 @@ StrokeWidthIndicator.propTypes = {
     disabled: PropTypes.bool.isRequired,
     format: PropTypes.oneOf(Object.keys(Formats)),
     onChangeStrokeColor: PropTypes.func.isRequired,
+    onChangeStrokeColor2: PropTypes.func.isRequired,
+    onChangeStrokeGradientType: PropTypes.func.isRequired,
     onChangeStrokeWidth: PropTypes.func.isRequired,
     onUpdateImage: PropTypes.func.isRequired,
     strokeWidth: PropTypes.number,
