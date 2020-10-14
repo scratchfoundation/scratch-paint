3f0e4fa914112b880ba0649a62f73d136688b122
rschamp <rschamp@media.mit.edu>
2020-10-14 14:30:41 +0000

    Build for 691e9ff9258c5c40e6a62404ba2c3228452813d2

--- hotfix/svg-iframe/playground.js
+++ hotfix/svg-iframe/playground.js
@@ -47290,16 +47290,29 @@ const measureSvgBBox = svgTag => new Promise((resolve, reject) => {
     // elements, similar to how Scratch 2.0 did measurement.
     const iframeElement = document.createElement('iframe');
     iframeElement.setAttribute('sandbox', 'allow-same-origin');
-    // eslint-disable-next-line no-undef
-    const svgBlob = new Blob([svgTag.outerHTML], {type: 'image/svg+xml'});
-    const iframeContent = URL.createObjectURL(svgBlob);
-    iframeElement.onload = () => {
-        const bbox = iframeElement.getSVGDocument().children[0].getBBox();
-        resolve(bbox);
-    };
-    iframeElement.src = iframeContent;
-    document.body.appendChild(iframeElement);
-    setTimeout(() => reject(new Error('Timed out loading SVG')), 30 * 1000);
+    
+    try {
+        const clonedTag = svgTag.cloneNode(/* deep */ true);
+        // eslint-disable-next-line no-undef
+        const svgBlob = new Blob([clonedTag.outerHTML], {type: 'image/svg+xml'});
+        const iframeContent = URL.createObjectURL(svgBlob);
+        let timeout = null;
+        iframeElement.onload = () => {
+            clearTimeout(timeout);
+            const bbox = iframeElement.getSVGDocument().children[0].getBBox();
+            resolve(bbox);
+            document.body.removeChild(iframeElement);
+        };
+        iframeElement.src = iframeContent;
+        document.body.appendChild(iframeElement);
+        timeout = setTimeout(() => {
+            reject(new Error('Timed out loading SVG'));
+            document.body.removeChild(iframeElement);
+        }, 30 * 1000);
+    } catch (e) {
+        reject(e);
+        document.body.removeChild(iframeElement);
+    }
 });
 
 module.exports = measureSvgBBox;
