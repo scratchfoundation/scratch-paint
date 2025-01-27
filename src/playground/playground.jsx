22036856b63d4666fbd3796ab211953453db0f88
Bogomil-Stoyanov <bogi2208@gmail.com>
2025-01-27 13:35:21 +0200

    refactor: [UEPR-63] changed dependencies

--- src/playground/playground.jsx
+++ src/playground/playground.jsx
@@ -182,10 +182,11 @@ class Playground extends React.Component {
     }
 
 }
-ReactDOM.render((
+const root = ReactDOM.createRoot(appTarget);
+root.render(
     <Provider store={store}>
         <IntlProvider>
             <Playground />
         </IntlProvider>
     </Provider>
-), appTarget);
+);
